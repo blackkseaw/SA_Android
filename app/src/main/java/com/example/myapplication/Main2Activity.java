5ec17f5e5758bd32f28a7280210edfbe10a88b0c
Fulano de Tal <fulanodetal@exemplo.br>
2021-10-25 12:50:28 -0300

    bottom nav

--- app/src/main/java/com/example/myapplication/Main2Activity.java
+++ app/src/main/java/com/example/myapplication/Main2Activity.java
@@ -1,14 +1,64 @@
 package com.example.myapplication;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.view.View;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class Main2Activity extends AppCompatActivity {
+
+    private BottomNavigationView bottomNavigationView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
+
+        BottomNavigationView bottomNav = findViewById(R.id.bottomNav);
+        bottomNav.setOnNavigationItemSelectedListener(navListener);
     }
+
+    private BottomNavigationView.OnNavigationItemSelectedListener navListener = new BottomNavigationView.OnNavigationItemSelectedListener() {
+        @Override
+        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+            Fragment selectFragment = null;
+
+            switch (item.getItemId()){
+                case R.id.nav_home:
+                    selectFragment = new HomeFragment();
+                    break;
+                case R.id.nav_parent:
+                    selectFragment = new ParentFragment();
+                    break;
+                case R.id.nav_about:
+                    selectFragment = new AboutFragment();
+                    break;
+                case R.id.nav_info:
+                    selectFragment = new InfoFragment();
+                    break;
+            }
+
+            getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
+                    selectFragment).commit();
+
+            return true;
+        }
+    };
+
+    public void  main2(View view){
+        Intent intent = new Intent(getApplicationContext(), Main2Activity.class);
+        startActivity(intent);
+    }
+
+    public void  main3(View view){
+        Intent intent = new Intent(getApplicationContext(), Main3Activity.class);
+        startActivity(intent);
+    }
+
 }
--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -1,24 +1,57 @@
 package com.example.myapplication;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+import androidx.fragment.app.Fragment;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.view.View;
+import android.widget.Toast;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainActivity extends AppCompatActivity {
 
-
+    private BottomNavigationView bottomNavigationView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-
-
+        BottomNavigationView bottomNav = findViewById(R.id.bottomNav);
+        bottomNav.setOnNavigationItemSelectedListener(navListener);
     }
+
+    private BottomNavigationView.OnNavigationItemSelectedListener navListener = new BottomNavigationView.OnNavigationItemSelectedListener() {
+        @Override
+        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+            Fragment selectFragment = null;
+
+            switch (item.getItemId()){
+                case R.id.nav_home:
+                    selectFragment = new HomeFragment();
+                    break;
+                case R.id.nav_parent:
+                    selectFragment = new ParentFragment();
+                    break;
+                case R.id.nav_about:
+                    selectFragment = new AboutFragment();
+                    break;
+                case R.id.nav_info:
+                        selectFragment = new InfoFragment();
+                    break;
+            }
+
+            getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
+                    selectFragment).commit();
+
+            return true;
+        }
+    };
 
     public void  main2(View view){
         Intent intent = new Intent(getApplicationContext(), Main2Activity.class);
